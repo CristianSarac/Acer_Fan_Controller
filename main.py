ccfa7d8553490fa612d0421e00805dfde347cc4a
cristi <sarac.cristian@gmail.com>
2015-07-22 15:29:44 +0200

    Added notifications

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
 
-from gi.repository import Gtk
+from gi.repository import Gtk,Notify
 import subprocess
 import os
 
@@ -11,6 +11,10 @@ class FanAcer(object):
         self.builder = Gtk.Builder()
         self.builder.add_from_file("FanAcer.glade")
         self.builder.connect_signals(self)
+         # lets initialise with the application name
+        Notify.init("FanAcer")
+        window = self.builder.get_object("mainWindow")
+        window.set_title("Acer Fan Controller")
 
     def run(self):
         self.builder.get_object("mainWindow").show_all()
@@ -18,16 +22,33 @@ class FanAcer(object):
         Gtk.main()
 
     def FanMaximum_clicked_cb(self, FanMaximum):
-
+        # Open a subprocces to execute the perl script
         pipe = subprocess.Popen(['/usr/bin/pkexec', "perl",os.getcwd()+"/acer_5750G_fan_controller.pl", "MAX"],
                                 stdout=subprocess.PIPE)
 
+        stdoutdata, stderrdata = pipe.communicate()
+
+        if (pipe.returncode==0):
+            # Send a notification on the speed of the Fans
+            n = Notify.Notification.new("FanAcer","Fan Speed set to Maximum")
+            n.set_timeout(Notify.EXPIRES_NEVER)
+            n.show()
 
     def FanNormal_clicked_cb(self, FanNormal):
-
+         # Open a subprocces to execute the perl script
         pipe = subprocess.Popen(
             ['/usr/bin/pkexec', "perl",os.getcwd()+ "/acer_5750G_fan_controller.pl", "NORMAL"],
             stdout=subprocess.PIPE)
+
+        stdoutdata, stderrdata = pipe.communicate()
+
+        if (pipe.returncode==0):
+            # Send a notification on the speed of the Fans
+            n = Notify.Notification.new("FanAcer","Fan Speed set to Normal")
+            n.set_timeout(Notify.EXPIRES_NEVER)
+            n.show()
+
+
 
     def close_window(self, *args):
         Gtk.main_quit()
